d642792831265c7538f4529042d350ea23ae2d1c
Dawar Mustaqeem <45918546+dmustaqeem@users.noreply.github.com>
2024-03-06 17:20:48 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,15 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
-// const fileUpload = require('express-fileupload');
-// const cors = require("cors");
-// require("dotenv").config();
 
 const app = express();
-// app.use(fileUpload());
-// const port = process.env.PORT;
-// const visits = require("./routes/visits");
-
 
 const mysql = require('mysql2');
 // create a new MySQL connection
 const connection = mysql.createConnection({
-
-  host: '45.77.222.55',
-  port:'9001',
-  user: 'clqzol2m3003u9ns3ffargnxm',
-  password: 'tg0WBGzjhlhstQv1wBZxldp3',
-  database: 'clqzol2m4003w9ns3gdtg7icn'
+    host: '104.238.131.15',
+    port: 9001,
+    user: 'clqzol2m3003u9ns3ffargnxm',
+    password: 'tg0WBGzjhlhstQv1wBZxldp3',
+    database: 'clqzol2m4003w9ns3gdtg7icn'
   
 });
 // connect to the MySQL database
